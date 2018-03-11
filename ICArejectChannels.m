54feecb5c9aaaa9cec76784c80aa058953f16caa
Ricardo <riddoddo@gmail.com>
2018-03-11 15:32:45 +0000

    Spectral powers

---
+++ ICArejectChannels.m
@@ -0,0 +1,27 @@
+% Reads datasets for which ICA has been previously applied, opening 
+% an EEGlab window to allow the user to reject individual components. Store
+% the new EEGlab dateset into another folder.
+
+%% ------------------------------------------------------------------------
+% Options you may want to change
+% -------------------------------------------------------------------------
+% Files
+data_folder = 'Data/icaData02-03-18/'; % Folder with the EEG data sets file
+file_type = '.set'; % File extension of the data set files
+save_folder = 'Data/pruneData02-03-18/'; % EEGlab datasets will be saved here
+
+%% ------------------------------------------------------------------------
+% Code
+% -------------------------------------------------------------------------
+mkdir(save_folder) % Create folder for the new datasets
+files = dir([data_folder, '*', file_type]);
+for file = files' % For every data set within the folder
+    EEG = pop_loadset('filename', file.name, 'filepath', file.folder);
+    EEG = pop_selectcomps(EEG, 1:size(EEG.icaweights, 1));
+    disp('Program paused, press a key...'); pause;
+    EEG = pop_subcomp(EEG);
+    
+    % Set name and save
+    EEG.setname = [file.name(1:end-length(file_type)), '_prune'];
+    EEG = pop_saveset(EEG, 'filename', [EEG.setname, '.set'], 'filepath', save_folder);
+end
---
+++ computeSpectralPowers.m
@@ -0,0 +1,58 @@
+% Compute spectral powers of all the EEG dataset files contained within a
+% folder. Save the powers to another folder as a text file.
+
+%% ------------------------------------------------------------------------
+% Options you may want to change
+% -------------------------------------------------------------------------
+% Files
+data_folder = 'Data/pruneData02-03-18/'; % Folder with the EEG data sets file
+file_type = '.set'; % File extension of the data set files
+save_folder = 'Data/powersPrune02-03-18/'; % Text files containing spectral 
+                                      % powers will be saved here
+
+% Frequencies for each spectral wave (in Hz)
+deltaFreq = [2,   4]; % Originally [1, 4]
+thetaFreq = [4,   8];
+alphaFreq = [8,  13];
+betaFreq  = [13, 30];
+gammaFreq = [30, 50]; % Originally [30, 80]
+
+%% ------------------------------------------------------------------------
+% Code
+% -------------------------------------------------------------------------
+mkdir(save_folder) % Create folder for output text files
+files = dir([data_folder, '*', file_type]);
+for file = files' % For every data file within the folder
+    EEG = pop_loadset('filename', file.name, 'filepath', file.folder);
+    [spectra,freqs] = spectopo(EEG.data, 0, EEG.srate);
+    
+    % Index for the relevant frequency bands
+    deltaIdx = find((freqs>deltaFreq(1)) & (freqs<deltaFreq(2)));
+    thetaIdx = find((freqs>thetaFreq(1)) & (freqs<thetaFreq(2)));
+    alphaIdx = find((freqs>alphaFreq(1)) & (freqs<alphaFreq(2)));
+    betaIdx  = find((freqs> betaFreq(1)) & (freqs< betaFreq(2)));
+    gammaIdx = find((freqs>gammaFreq(1)) & (freqs<gammaFreq(2)));
+    
+    
+    % Compute absolute power
+    deltaPower = 10^(mean(spectra(deltaIdx))/10);
+    thetaPower = 10^(mean(spectra(thetaIdx))/10);
+    alphaPower = 10^(mean(spectra(alphaIdx))/10);
+    betaPower  = 10^(mean(spectra(betaIdx))/10);
+    gammaPower = 10^(mean(spectra(gammaIdx))/10);
+    
+    % Compute relative powers
+    summPower = deltaPower + thetaPower + alphaPower + betaPower + gammaPower;
+    deltaRelPwr = deltaPower / summPower;
+    thetaRelPwr = thetaPower / summPower;
+    alphaRelPwr = alphaPower / summPower;
+    betaRelPwr  = betaPower  / summPower;
+    gammaRelPwr = gammaPower / summPower;
+    
+    % Save data to files
+    outdatamat = [deltaPower, thetaPower, alphaPower, betaPower, gammaPower;
+        deltaRelPwr, thetaRelPwr, alphaRelPwr, betaRelPwr, gammaRelPwr];
+    outname = [file.name(1:end-length(file_type)), '.txt'];
+    dlmwrite([save_folder, outname], outdatamat)
+end
+close(figure(1)) % Close output figure
