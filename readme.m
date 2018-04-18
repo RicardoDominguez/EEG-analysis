9e2c086b1cc5b302e3cb9aa598197e1f01e4e714
Ricardo <riddoddo@gmail.com>
2018-04-18 12:24:16 +0100

    Ignore files, add to preprocessing ability to remove beggining/end seconds, make plot conditional

---
+++ readme.m
@@ -0,0 +1,6 @@
+% Process
+% 1. Put .easy files from enobio into 'rawFilesDate'
+% 2. Run convert_rawFile_to_rawData to create 'rawDataDate'
+%   This folders contains text files but only with the relevant data
+%   columns
+% 3. 
